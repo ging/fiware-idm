d6cb9e24c25eb41310bf9645f31331bbd7dd088e
agaldemas <alain.galdemas@gmail.com>
2018-07-03 14:30:05 +0200

    for pg remove CHARSET

--- migrations/20180612134640-CreateEidasTable.js
+++ migrations/20180612134640-CreateEidasTable.js
@@ -9,46 +9,46 @@ module.exports = {
                   defaultValue: Sequelize.UUIDV4,
                   primaryKey: true
                 }, support_contact_person_name: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "support_contact_person_name"}}
                 }, support_contact_person_surname: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "support_contact_person_surname"}}
                 }, support_contact_person_email: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "support_contact_person_email"}}
                 }, support_contact_person_telephone_number: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "support_contact_person_telephone_number"}}
                 }, support_contact_person_company: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "support_contact_person_company"}}
                 }, technical_contact_person_name: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "technical_contact_person_name"}}
                 }, technical_contact_person_surname: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "technical_contact_person_surname"}}
                 }, technical_contact_person_email: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "technical_contact_person_email"}}
                 }, technical_contact_person_telephone_number: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "technical_contact_person_telephone_number"}}
                 }, technical_contact_person_company: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "technical_contact_person_company"}}
                 },organization_name: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "organization_name"}}
                 }, organization_url: {
-                  type: Sequelize.STRING(255) + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
+                  type: Sequelize.STRING(255), // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci',
                   validate: { notEmpty: {msg: "organization_url"}}
                 }, oauth_client_id: {
                     type: Sequelize.UUID,
                     onDelete: 'CASCADE',
                     unique: true,
-                    references: {   
+                    references: {
                         model: 'oauth_client',
                         key: 'id'
                     }

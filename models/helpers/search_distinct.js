1b44be0a9ec50ff2d72b681e564e45099eeaca10
agaldemas <alain.galdemas@gmail.com>
2018-07-04 16:41:35 +0200

    Added quote for user => "user" to be OK with postgres

--- models/helpers/search_distinct.js
+++ models/helpers/search_distinct.js
@@ -17,7 +17,7 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
 module.exports = function(table, join_table, entity_id, entity_type, key, offset_value, count_rows, role) {
 	var select = 'SELECT DISTINCT '+table+'.'+join_table+'_id,'
 	if (join_table === 'user') {
-		select = select + 'user.id, user.username, user.image, user.gravatar, user.email'
+		select = select + '"user".id, "user".username, "user".image, "user".gravatar, "user".email'
 	} else if (join_table === 'organization') {
 		select = select + 'organization.id, organization.name, organization.image, organization.description'
 	} else if (join_table === 'oauth_client') {
@@ -26,15 +26,16 @@ module.exports = function(table, join_table, entity_id, entity_type, key, offset
 
 	var from = 'FROM ' + table + ''
 	if (join_table === 'user') {
-		var join = 'RIGHT JOIN (SELECT * FROM '+join_table+' WHERE username LIKE :key) AS '+join_table+''
+//    join_table = '"' + join_table + '"'
+		var join = 'RIGHT JOIN (SELECT * FROM "'+join_table+'" WHERE "username" LIKE :key) AS "'+join_table+'"'
 	} else {
 		var join = 'RIGHT JOIN (SELECT * FROM '+join_table+' WHERE name LIKE :key) AS '+join_table+''
 	}
-	var on = 'ON '+table+'.'+join_table+'_id='+join_table+'.id'
+	var on = 'ON '+table+'.'+join_table+'_id="'+join_table+'".id'
 	var where = 'WHERE '+entity_type+'_id=:entity_id'
 	var and = ''
 	if (table==='role_assignment') {
-		if (['user', 'organization'].includes(join_table) && !role) {
+		if (['user', 'organization', '"user"'].includes(join_table) && !role) {
 			and = 'AND '+join_table+'_id IS NOT NULL'
 		} else if (join_table === 'oauth_client' && role !== undefined) {
 			if (role === 'other')  {

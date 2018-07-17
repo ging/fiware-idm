e8032503e0cbcfde2df830fbb1e79b05ab018f86
agaldemas <alain.galdemas@gmail.com>
2018-07-17 15:12:02 +0200

    user to quote in request "user"

--- models/helpers/search_identity.js
+++ models/helpers/search_identity.js
@@ -5,26 +5,26 @@ var config = require('../../config.js').database
 var Sequelize = require('sequelize');
 
 // Use BBDD Mysql
-var sequelize = new Sequelize(config.database, config.username, config.password, 
-  { 
+var sequelize = new Sequelize(config.database, config.username, config.password,
+  {
     host: config.host,
     dialect: config.dialect
-  }      
+  }
 );
 
 
-// Helper to find info about pep proxy or user 
+// Helper to find info about pep proxy or user
 exports.search_pep_or_user = function(id) {
-    var query = `SELECT email, 'user' as Source FROM user WHERE email=:id
+    var query = `SELECT email, 'user' as Source FROM "user" WHERE email=:id
                  UNION ALL
                  SELECT id, 'pep_proxy' as Source FROM pep_proxy WHERE id=:id;`
 
     return sequelize.query(query, {replacements: {id: id}, type: Sequelize.QueryTypes.SELECT})
 }
 
-// Helper to find info about iot or user 
+// Helper to find info about iot or user
 exports.search_iot_or_user = function(id) {
-    var query = `SELECT email, 'user' as Source FROM user WHERE email=:id
+    var query = `SELECT email, 'user' as Source FROM "user" WHERE email=:id
                  UNION ALL
                  SELECT id, 'iot' as Source FROM iot WHERE id=:id;`
 

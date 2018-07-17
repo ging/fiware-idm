774fe45e1c20b70ba6741f111cd139a14109b967
agaldemas <alain.galdemas@gmail.com>
2018-07-17 17:37:28 +0200

    test again

--- models/helpers/search_identity.js
+++ models/helpers/search_identity.js
@@ -17,17 +17,14 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
 exports.search_pep_or_user = function(id) {
     var query = "SELECT email, 'user' as Source FROM \"user\" WHERE email=:id"
     + " UNION ALL SELECT id, 'pep_proxy' as Source FROM pep_proxy WHERE id=:id;"
-    var res = sequelize.query(query, {replacements: {id: id}, type: Sequelize.QueryTypes.SELECT});
-    console.log("-----------------> query: ", query);
-    console.log("-----------------> res: ", res);
-    return res; //sequelize.query(query, {replacements: {id: id}, type: Sequelize.QueryTypes.SELECT})
+
+    return sequelize.query(query, {replacements: {id: id}, type: Sequelize.QueryTypes.SELECT})
 }
 
 // Helper to find info about iot or user
 exports.search_iot_or_user = function(id) {
-    var query = `SELECT email, 'user' as Source FROM "user" WHERE email=:id
-                 UNION ALL
-                 SELECT id, 'iot' as Source FROM iot WHERE id=:id;`
+    var query = "SELECT email, 'user' as Source FROM \"user\" WHERE email=:id"
+    + " UNION ALL SELECT id, 'iot' as Source FROM iot WHERE id=:id;"
 
     return sequelize.query(query, {replacements: {id: id}, type: Sequelize.QueryTypes.SELECT})
 }

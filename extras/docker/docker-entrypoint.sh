5d45c12d52c11916e40275501ef9871b30ed6369
Jason Fox <jason.fox@fiware.org>
2018-07-19 16:35:13 +0200

    Retrieve selected environment variables from a file to implement Docker secrets support
    ## Docker Secrets
    
    As an alternative to passing sensitive information via environment variables, `_FILE` may be appended to the listed environment variables, causing the initialization script to load the values for those variables from files present in the container. In particular, this can be used to load passwords from Docker secrets stored in `/run/secrets/<secret_name>` files.
    
    *  `IDM_SESSION_SECRET_FILE`
    *  `IDM_ENCRYPTION_KEY_FILE`
    *  `IDM_DB_PASS_FILE`
    *  `IDM_DB_USER_FILE`
    *  `IDM_EX_AUTH_DB_USER_FILE`
    *  `IDM_EX_AUTH_DB_PASS_FILE`
    
    For example
    
    ```
    docker run --name some-idm -e IDM_SESSION_SECRET_FILE=/run/secrets/session -d fiware-idm:tag
    ```

--- extras/docker/docker-entrypoint.sh
+++ extras/docker/docker-entrypoint.sh
@@ -4,6 +4,40 @@
 service postfix restart
 
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'IDM_SESSION_SECRET'
+file_env 'IDM_ENCRYPTION_KEY'
+file_env 'IDM_DB_PASS'
+file_env 'IDM_DB_USER'
+file_env 'IDM_ADMIN_ID'
+file_env 'IDM_ADMIN_USER'
+file_env 'IDM_ADMIN_EMAIL'
+file_env 'IDM_ADMIN_PASS' 
+file_env 'IDM_EX_AUTH_DB_USER'
+file_env 'IDM_EX_AUTH_DB_PASS'
+
+
 if [[ $IDM_SESSION_SECRET ==  "nodejs_idm" ]] || [[ IDM_ENCRYPTION_KEY == "nodejs_idm'" ]]; then
 	echo "***********************************************"
 	echo "WARNING: Encryption keys must be changed if you are using the IDM in a production Environment"

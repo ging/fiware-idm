f3c9c09fe5dd2b50e8554ae4ca5fa02bfe8f4ac5
agaldemas <alain.galdemas@gmail.com>
2018-10-11 15:14:12 +0200

    add console.log

--- extras/docker/config_database.js
+++ extras/docker/config_database.js
@@ -5,12 +5,12 @@ var exec = require('child_process').exec;
 var Sequelize = require('sequelize');
 
 // Use BBDD Mysql
-var sequelize = new Sequelize(database.database, database.username, database.password, 
-{ 
+var sequelize = new Sequelize(database.database, database.username, database.password,
+{
     host: database.host,
     dialect: database.dialect,
     port: (database.port !== 'default') ? database.port : undefined
-}      
+}
 );
 
 sequelize
@@ -20,9 +20,10 @@ sequelize
     process.exit()
 })
 .catch(err => {
+  console.log('config db error:', err);
 
     if (err.original.code === 'ER_BAD_DB_ERROR') {
-        exec('npm run-script create_db', function(error, stdout, stderr){ 
+        exec('npm run-script create_db', function(error, stdout, stderr){
             if (error) {
                 console.log("Unable to create database: ", err);
                 process.exit()
@@ -30,7 +31,7 @@ sequelize
 
             console.log("Database created")
 
-            exec('npm run-script migrate_db', function(error, stdout, stderr){ 
+            exec('npm run-script migrate_db', function(error, stdout, stderr){
                 if (error) {
                     console.log("Unable to migrate database: ", err);
                     process.exit()
@@ -38,15 +39,15 @@ sequelize
 
                 console.log("Database migrated")
 
-                exec('npm run-script seed_db', function(error, stdout, stderr){ 
+                exec('npm run-script seed_db', function(error, stdout, stderr){
                     if (error) {
                         console.log("Unable to seed database: ", err);
 
                     }
-                    if (process.env.IDM_ADMIN_PASS === undefined || process.env.IDM_ADMIN_USER === undefined || 
+                    if (process.env.IDM_ADMIN_PASS === undefined || process.env.IDM_ADMIN_USER === undefined ||
                        process.env.IDM_ADMIN_EMAIL === undefined || process.env.IDM_ADMIN_PASS === undefined) {
                         console.log( "****************");
-                        console.log( "WARNING: Seeding database with an admin user using default credentials." + 
+                        console.log( "WARNING: Seeding database with an admin user using default credentials." +
                         "This user must be deleted when running on a production instance");
                         console.log( "****************");
                     }

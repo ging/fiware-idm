15724de45da4df89bd141d0813db7d8c741b5b7f
agaldemas <alain.galdemas@gmail.com>
2019-01-25 19:05:08 +0100

    fix issue for postgres comment // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci'

--- migrations/20190116101526-CreateSignOutUrl.js
+++ migrations/20190116101526-CreateSignOutUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
       queryInterface.addColumn('oauth_client', 'redirect_sign_out_uri', {
-        type: Sequelize.STRING(2000)  + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci'
+        type: Sequelize.STRING(2000)  // + ' CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci'
       })
     ])
   },

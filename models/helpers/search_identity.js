2bab31ad20ac025b3151000df38974f8829f46d3
agaldemas <alain.galdemas@gmail.com>
2019-01-28 19:49:32 +0100

    rework query preparation

--- models/helpers/search_identity.js
+++ models/helpers/search_identity.js
@@ -17,9 +17,9 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
 
 // Helper to find info about pep proxy or user
 exports.search_pep_or_user = function(id) {
-    var query = `SELECT email, 'user' as Source FROM "user" WHERE email=:id
-                 UNION ALL
-                 SELECT id, 'pep_proxy' as Source FROM pep_proxy WHERE id=:id;`
+    var query = "SELECT email, 'user' as Source FROM \"user\" WHERE email=:id"
+                 + " UNION ALL "
+                 + "SELECT id, 'pep_proxy' as Source FROM pep_proxy WHERE id=:id;"
 
     if (sequelize.getDialect() === 'mysql') {
          query = query.replace('"','`')
@@ -31,9 +31,9 @@ exports.search_pep_or_user = function(id) {
 
 // Helper to find info about iot or user
 exports.search_iot_or_user = function(id) {
-    var query = `SELECT email, 'user' as Source FROM "user" WHERE email=:id
-                 UNION ALL
-                 SELECT id, 'iot' as Source FROM iot WHERE id=:id;`
+    var query = "SELECT email, 'user' as Source FROM \"user\" WHERE email=:id
+                 + " UNION ALL "
+                 + "SELECT id, 'iot' as Source FROM iot WHERE id=:id;"
 
     if (sequelize.getDialect() === 'mysql') {
          query = query.replace('"','`')

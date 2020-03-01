702c457fa0b10e9f3ee764f78bf2970189132a93
alex <apozohue10@gmail.com>
2020-03-01 13:46:48 +0100

    DTLS

--- controllers/coap/oauth2.js
+++ controllers/coap/oauth2.js
@@ -1,5 +1,8 @@
 const debug = require('debug')('idm:coap_controller');
 const OauthServer = require('oauth2-server'); //eslint-disable-line snakecase/snakecase
+const packet      = require('coap-packet')
+    , parse       = packet.parse
+    , generate    = packet.generate
 const Request = OauthServer.Request;
 const Response = OauthServer.Response;
 
@@ -10,44 +13,97 @@ const oauth_server = new OauthServer({
   debug: true,
 });
 
-exports.paths = function(req, res) {
-  const pet = req.method.toUpperCase() + ' ' + req.url;
+exports.paths = async function(req) {
+  req = parse(req)
+  let pet;
+  
+  req.body = JSON.parse(req.payload.toString('utf8'))
+  delete req.payload
+  req.query = {};
+  req.headers = {};
 
-  switch (pet) {
-    case 'POST /oauth2/token':
-      token(req, res);
+  switch (req.code) {
+    case '0.01':
+      req.method = 'GET'
+      break;
+    case '0.02':
+      req.method = 'POST'
       break;
     default:
-      res.end('Not Found');
+      break;
   }
+
+  for (var i = req.options.length - 1; i >= 0; i--) {
+    req.options[i]['value'] = req.options[i]['value'].toString('utf8')
+
+    if (req.options[i]['name'] == 'Uri-Path') {
+      pet = req.method + ' ' + req.options[i]['value'];
+    }
+  }
+
+  let response;
+  switch (pet) {
+    case 'POST /oauth2/token':
+      response = await token(req);
+      break;
+    default:
+      let coap_options =  {
+        code: '4.00'
+      }
+      response = generate(coap_options)
+      break;
+  }
+  return response
 };
 
 // POST /oauth2/token -- Function to handle token requests
 function token(req, res) {
   debug(' --> token');
 
-  const payload = JSON.parse(req.payload.toString('utf8'));
+  //const payload = JSON.parse(req.payload.toString('utf8'));
 
   // Add variables to COAP request beacause are mandatory in OAuth dependency
-  req.query = {};
-  req.body = payload;
-  delete req.payload;
+  // req.query = {};
+  // req.body = payload;
+  // delete req.payload;
 
   const request = new Request(req);
-  const response = new Response(res);
-
-  oauth_server
+  const response = new Response();
+  
+  return oauth_server
     .token(request, response)
     .then(function(result) {
-      const response = {
-        access_token: result.access_token,
-        refresh_token: result.refresh_token,
-      };
-      res.code = '2.01';
-      res.end(JSON.stringify(response));
+
+      let payload = {
+        "access_token": result.access_token,
+        "refresh_token": result.refresh_token
+      }
+      
+      let coap_options =  {
+        code: '2.01',
+        token: req.token,
+        payload: new Buffer(JSON.stringify(payload)),
+        options: [{
+            name: 'Content-Format',
+            value: new Buffer('50')
+        }]
+      }
+      return generate(coap_options)
     })
     .catch(function(error) {
       debug('Error ', error);
+
+      let payload = 'error'
+      
+      let coap_options =  {
+        code: '2.01',
+        token: req.token,
+        payload: new Buffer(payload),
+        options: [{
+            name: 'Content-Format',
+            value: new Buffer('0')
+        }]
+      }
 
       if (error.code) {
         let num = error.code;
@@ -58,11 +114,11 @@ function token(req, res) {
           num = parseInt(num / 10); // eslint-disable-line snakecase/snakecase
         }
         digits.reverse().splice(1, 0, '.');
-        res.code = digits.join('');
+        coap_options.code = digits.join('');
       } else {
-        res.code = '5.00';
+        coap_options.code = '5.00';
       }
 
-      res.end('error');
+      return generate(coap_options)
     });
 }

d7be0511462fd845f7e665f3ba084704b7301623
alex <apozohue10@gmail.com>
2020-03-02 02:20:42 +0100

    FIx

--- seeders/201802190030-FillPep.js
+++ seeders/201802190030-FillPep.js
@@ -1,22 +1,13 @@
 'use strict';
 
-var crypto = require('crypto');
-var salt = crypto
-  .randomBytes(16)
-  .toString('hex')
-  .slice(0, 16);
-
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('pep_proxy', [
       {
-        id: 'pep_proxy_0e2ff2c9-7bf4-46cd-81dd-58c871a27796',
-        password: crypto
-          .createHmac('sha1', salt)
-          .update(process.env.IDM_ADMIN_PASS || 'd7ae2119-2793-400d-9806-4d5cb2e3c74e')
-          .digest('hex'),
+        id: 'pep_proxy_bb676cb0-51d2-4016-aba5-48ea3af09801',
+        password: '1d5a3fa85e75f14f21a287bbb53b3cf059bc7b59',
         oauth_client_id: '3771a537-84d4-4985-8cc3-bd24703138c3',
-        salt: salt,
+        salt: '86cc70908eb242f1',
       },
     ]);
   },

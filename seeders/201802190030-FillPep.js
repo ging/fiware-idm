764522ad89b22f3ab6dec54999732da65519c9f2
alex <apozohue10@gmail.com>
2020-03-02 01:28:01 +0100

    FIx seeder

--- seeders/201802190030-FillPep.js
+++ seeders/201802190030-FillPep.js
@@ -5,7 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('pep_proxy', [
       {
         id: 'pep_proxy_0e2ff2c9-7bf4-46cd-81dd-58c871a27796',
-        password: 'pep_proxy_d7ae2119-2793-400d-9806-4d5cb2e3c74e',
+        password: 'd7ae2119-2793-400d-9806-4d5cb2e3c74e',
         oauth_client_id: '3771a537-84d4-4985-8cc3-bd24703138c3',
       },
     ]);

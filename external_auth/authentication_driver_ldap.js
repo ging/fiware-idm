83b7933ff12b22d3a946e626f01d9d4fbcefc682
Jason Fox <jason.fox@fiware.org>
2020-10-27 07:34:12 +0100

    Minimal fix, run linter and prettier and fix remaining errors.

--- external_auth/authentication_driver_ldap.js
+++ external_auth/authentication_driver_ldap.js
@@ -4,85 +4,82 @@ const config = config_service.get_config();
 const external_auth = config.external_auth_ldap;
 
 // LDAP library, documented at http://ldapjs.org/client.html
-var ldap = require('ldapjs');
+const ldap = require('ldapjs');
 
 const debug = require('debug')('idm:external_auth_ldap');
 
 // MW to see if user is registered
 exports.authenticate = function (username, password, callback) {
   debug('--> authenticating external user');
-  
 
   // Search the user
-  var client = ldap.createClient({
-      url: 'ldap://' + external_auth.database.host + ':' + external_auth.database.port
+  const client = ldap.createClient({
+    url: 'ldap://' + external_auth.database.host + ':' + external_auth.database.port
   });
 
-  client.bind(external_auth.database.reader_dn, external_auth.database.reader_password, function(err) {
+  client.bind(external_auth.database.reader_dn, external_auth.database.reader_password, function (err) {
     if (err) {
       debug('--> ERROR LDAP', err);
       callback(err);
     }
 
-    var filter = '(uid=' + username + ')';
-    var suffix = external_auth.database.suffix;
+    const filter = '(uid=' + username + ')';
+    const suffix = external_auth.database.suffix;
 
-    client.search(suffix, {filter: filter, scope:"sub"}, (err, searchRes) => {
-        var searchList = [];
-        
-        if (err) {
-          debug('--> ERROR LDAP', err);
+    client.search(suffix, { filter, scope: 'sub' }, (err, search_result) => {
+      const search_list = [];
+
+      if (err) {
+        debug('--> ERROR LDAP', err);
+        callback(err);
+      }
+
+      search_result.on('searchEntry', (entry) => {
+        debug('--> Found entry (user): ' + entry);
+        search_list.push(entry);
+      });
+
+      search_result.on('error', (err) => {
+        debug('--> Search failed with ' + err);
+        callback(err);
+      });
+
+      /* eslint-disable-next-line no-unused-vars */
+      search_result.on('end', (return_value) => {
+        debug('--> Search results length: ' + search_list.length);
+
+        for (let i = 0; i < search_list.length; i++) {debug('--> User DN:' + search_list[i].objectName);}
+
+        //debug('--> Search retval:' + retVal);
+
+        if (search_list.length === 1) {
+          client.bind(search_list[0].objectName, password, function (err) {
+            if (err) {
+              debug('--> LDAP Bind with real credential error: ' + err);
+              callback(new Error('invalid'));
+            } else {
+              debug('--> Bind with real credential is a success');
+              find_local_user(search_list[0], function (local_user) {
+                callback(null, local_user);
+              });
+            }
+          });
+        } else if (search_list.length === 0) {
+          debug('--> LDAP User not found');
+          callback(new Error('user_not_found'));
+        } else {
+          debug('--> ERROR LDAP No unique user to bind');
           callback(err);
         }
-        
-        searchRes.on("searchEntry", (entry) => {
-          debug('--> Found entry (user): ' + entry);
-          searchList.push(entry);
-        });
-
-        searchRes.on("error", (err) => {
-          debug('--> Search failed with ' + err);
-          callback(err);
-        });
-        
-        searchRes.on("end", (retVal) => {
-          debug('--> Search results length: ' + searchList.length);
-          
-          for(var i=0; i<searchList.length; i++) 
-            debug('--> User DN:' + searchList[i].objectName);
-          
-          //debug('--> Search retval:' + retVal);          
-          
-          if (searchList.length === 1) {          
-            client.bind(searchList[0].objectName, password, function(err) {
-              if (err) {
-                debug('--> LDAP Bind with real credential error: ' + err);
-                callback(new Error('invalid'));
-              }
-              else {
-                debug('--> Bind with real credential is a success');
-                find_local_user(searchList[0], function (local_user) {
-                  callback(null, local_user);
-                });
-              }
-            });
-          } else if (searchList.length === 0) {
-            debug('--> LDAP User not found');
-            callback(new Error('user_not_found'));
-          } else {
-            debug('--> ERROR LDAP No unique user to bind');
-            callback(err);
-          }
-        });      
-    });   
-  });   
+      });
+    });
+  });
 };
 
 function find_local_user(user, callback) {
-  
-  let userId = getUserAttribute(user, external_auth.database.idAttribute);
+  const user_id = get_user_attribute(user, external_auth.database.idAttribute);
 
-  debug('--> searching local user with id: ', external_auth.id_prefix + userId);
+  debug('--> searching local user with id: ', external_auth.id_prefix + user_id);
   models.user
     .find({
       attributes: [
@@ -99,7 +96,7 @@ function find_local_user(user, callback) {
         'starters_tour_ended'
       ],
       where: {
-        id: external_auth.id_prefix + userId
+        id: external_auth.id_prefix + user_id
       }
     })
     .then(function (local_user) {
@@ -122,17 +119,17 @@ function find_local_user(user, callback) {
 function create_local_user(user, callback) {
   debug('--> creating local user');
 
-  let userId = getUserAttribute(user, external_auth.database.idAttribute);
-  let username = getUserAttribute(user, external_auth.database.usernameAttribute);
-  let userMail = getUserAttribute(user, external_auth.database.emailAttribute);
+  const user_id = get_user_attribute(user, external_auth.database.idAttribute);
+  const username = get_user_attribute(user, external_auth.database.usernameAttribute);
+  const user_mail = get_user_attribute(user, external_auth.database.emailAttribute);
 
   // TODO: update user values if changed in external database
 
   // Build a row and validate it
   const local_user = models.user.build({
-    id: external_auth.id_prefix + userId,
-    username: username,
-    email: userMail,
+    id: external_auth.id_prefix + user_id,
+    username,
+    email: user_mail,
     password: 'none',
     date_password: new Date(new Date().getTime()),
     enabled: true
@@ -153,10 +150,10 @@ function create_local_user(user, callback) {
     });
 }
 
-function getUserAttribute(user, attribute) {
+function get_user_attribute(user, attribute) {
   debug('--> Getting attribute', attribute);
-  for (var a of user.attributes) {
-    if (a.type === attribute) 
-      return a.vals[0];
+  for (const a of user.attributes) {
+    if (a.type === attribute) {return a.vals[0];}
   }
+  return undefined;
 }

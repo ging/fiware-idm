e97cab933e2e62b97f5a79106da0a3b26faf2127
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-10-20 18:15:21 +0200

    Reset redirect URL to avoid redirect by default

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.host = 'http://172.17.0.1:3000';
 
 config.debug = false;
 config.registration = {
-  redirect: 'https://plattform.ki-marktplatz.com/Group/Welcome/Willkommen',
+  redirect: '',
   extension: '/?new_user=1'
 }
 // HTTPS enable
